f6db330bc3021828aa8a9e71ac6a8b7881cb2389
Christopher Hacia <chris@chrishacia.com>
2013-05-01 11:59:01 -0700

    Various patching done, removed ability to check for current window position as in will it fall off page.. fixed a handful of otherwise noteable bugs.. and added the ability to switch between a fixed element and absolute element for those tricky relative based parents

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // Website: http://www.chrishacia.com/scripts/tippedOff
 // Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 // Author: Christopher Hacia
-// Version: 0.9
+// Version: 0.92
 //
 //
 // Useage:
@@ -23,44 +23,55 @@
 //I have a very specific method of delivering JSON in my projects to keep things orderly. Below is an example of that in particular.
 //If you do not like this format, you can always change it and the section of code below that coresponds to mapping the data to the right thing.
 //which is currently line 63: var help_obj = tippedOff_obj.data.help_text;
-var tippedOff_obj = {"status": "SUCCESS","code": null,"message": null,"data": {"help_text": {"deletion_policy": "Allow delete from Zetta when deleted locally:<br><br>This option allows local deletions to be propagated to your ZettaMirror volume; if an item is detected to have been deleted, it will also be deleted from Zetta, and held in snapshots for the duration of your retention policy. While the data is held in snapshots it is still fully recoverable by browsing to the appropriate snapshot date. Once the retention period has elapsed the data will be fully deleted from your ZettaMirror volume.<br><br>Disabling this option puts the system into archive mode where any data deleted locally will live on ZettaMirror in primary storage forever. Your retention policy will function solely as file versioning where changes in data will be the only item captured in snapshots.<br><br>","sql_backup_path": "Using Zetta for backing up SQL Server requires temporarily backing it up to a local destination in your environment. The plugin writes a full backup of the SQL instance to disk so adequate space is needed to prevent backup failure. The plugin will delete the backup after sync completion so there is no need to provision more space than is required for a single full backup.<br><br>Although we generate full backups each time, we only move the incremental changes through the use of our sub-file change detection technology.<br><br>","sql_backup_default": "Check this box if the instance of SQL is the default instance name.<br><br>You can verify the instance name by opening services and checking what appears in brackets after the 'SQL Server' service. If the name in brackets is MSSQLSERVER your instance is the default name, anything else is a named instance and will have to be itemized in the Non-Default Instance configuration of ZettaMirror.<br><br>","sql_backup_local_instance": "If your instance of SQL is a non-default name please list out the names used with semicolon delimitation for multiple names.<br><br>You can verify the instance name by opening services and checking what appears in brackets after the 'SQL Server' service. If the name in brackets is MSSQLSERVER your instance is the default name, if the name is anything else your instance is Non-Default and the names must be listed here.","systemstate_backup_path": "Using Zetta for backing up system state requires temporarily backing it up to a local destination in your environment. Enter a target destination for the system state backup to be written to. A system state backup is generally between 10-20GB so it is important to provision adequate space to avoid backup failure. The plugin will ensure that no more than one system state backup is stored simultaneously on the local machine.<br><br>","invalid_key": null}},"actions": null};
-var tippedOff_timer = null;
+var tippedOff_obj = {"status": "SUCCESS","code": null,"message": null,"data": {"help_text": {"help_ref_id_1": "some general help text","help_ref_id_2": "some more general help text"}},"actions": null};
+var tippedOff_timer = null;//global var for the functions to see if the timer is running or not, to try and prevent overlap and other nasty UI side effects
+var currentMousePos = { x: -1, y: -1 };
 (function($)
 {
-	$.fn.tippedOff = function(settings)
+	$.fn.tippedOff = function(obj, settings)
 	{
 		var config = $.extend({
 			'top':0,
 			'left':0,
-			'width':'550px',
-			'height':'200px',
-			'styleClass':'round_edges blue_border',
+			'width':'400px',
+			'height':'100px',
+			'styleClass':'round_edges green_border',
 			'wait':1300,
-			'closeWait':1000
+			'closeWait':1000,
+			'showFixed':false
 		}, settings);
 
-		//var $elem = $(selector);
+		if($('.tippedOff_msg_fixed').length > 0)
+		{
+			$('.tippedOff_msg_fixed').remove();
+		}
+		$('body').append('<div class="tippedOff_msg_fixed '+config.styleClass+'" style="height:'+config.height+';width:'+config.width+';"></div>');
 
 		return this.each(function()
 		{
+			if(obj == null || obj == undefined || obj == '')
+			{
+				alert('Missing Help Text Object.');
+				return false;
+			}
 			$this = $(this);
 			$this.addClass('tippedOff_hover');
 			$this.attr('data-wait', config.wait);
 			$this.attr('data-cwait', config.closeWait);
 			$this.attr('data-offsetLeft', config.left);
 			$this.attr('data-offsetTop', config.top);
+			$this.attr('data-showFixed', config.showFixed);
 			var tippedHTML = '';
 				tippedHTML += '<div class="tippedOff_msg '+config.styleClass+'" style="height:'+config.height+';width:'+config.width+';">';
-
 			if($this.data('ref') == null || $this.data('ref') == undefined || $this.data('ref') == '')
 			{
 				tippedHTML += 'Message Reference ID Missing or not Found.';
 			}
 			else
 			{
-				if(tippedOff_obj.status == "SUCCESS")
+				if(obj.status == "SUCCESS")
 				{
-					var help_obj = tippedOff_obj.data.help_text;
+					var help_obj = obj.data.help_text;
 					if(help_obj.hasOwnProperty($this.data('ref')))
 					{
 						tippedHTML += help_obj[$this.data('ref')];
@@ -75,24 +86,8 @@ var tippedOff_timer = null;
 					tippedHTML += 'Tipped Off help text object missing or invalid.';
 				}
 			}
-
 			tippedHTML += '</div>';
-
 			$this.after(tippedHTML);
-
-/*
-			$this
-				.mouseenter(function(){tippedOffShow($this);})
-				.mouseleave(function(){tippedOffHide($this.data('cwait'));})
-				.click(function(e)
-				{
-					e.stopImmediatePropagation();
-					tippedOff_timer = setTimeout(function()
-					{
-						tippedOffShow($this);
-					}, $(this).data('wait'));
-				});
-*/
 		});
 	};
 })(jQuery);
@@ -104,43 +99,106 @@ $('.tippedOff_hover').live({
 				'mouseleave':function(){tippedOffHide($(this).data('cwait'));},
 				'click':function(e){e.stopImmediatePropagation();tippedOffShow($(this), false);}
 });
-$('.tippedOff_msg').live({
+$('.tippedOff_msg, .tippedOff_msg_fixed').live({
 				'mouseenter':function(){clearTippedOffTimer();},
-				'mouseleave':function(){tippedOffHide($(this).prev('span').data('cwait'));}
+				'mouseleave':function(){tippedOffHide($(this).prev('span').data('cwait'));},
+				'click':function(e){e.stopImmediatePropagation();tippedOffHide(false);}
 });
-function tippedOffShow(elem, wait)
+function tippedOffShow(elem, wait, showFixed)
 {
 	$elem = elem;
+
+	//alert('hello')
 	$msg_elem = $elem.next('.tippedOff_msg');
 	var elPos = tippedOffElemPosition($elem);
 	var offsetX = $elem.data('offsetLeft');
 	var offsetY = $elem.data('offsetTop');
+	var position = "absolute";
 
-	elPos.setting_left = elPos.setting_left+offsetX;
-	elPos.setting_top = elPos.setting_top+offsetY;
+	if( $elem.data('showFixed') == false )
+	{
+		elPos.setting_left = elPos.setting_left + offsetX;
+		elPos.setting_top = elPos.setting_top + offsetY;
+		position = "absolute";
+	}
+	else
+	{
+/*
+		elPos.setting_left = Math.min( window.event.clientX, $elem.offset().left );
+		elPos.setting_top = Math.min( window.event.clientY, $elem.offset().top) + $elem.height(); // elPos.setting_top+offsetY;
+*/
+		elPos.setting_left = Math.min( currentMousePos.x, $elem.offset().left );
+		elPos.setting_top = Math.min( currentMousePos.y, $elem.offset().top) + $elem.height(); // elPos.setting_top+offsetY;
+		position = "fixed";
+	}
 
 	if(tippedOff_timer !== null)
 	{
 		clearTippedOffTimer();
 	}
-	if(wait !== false)
+
+	if( $msg_elem.is(':visible')  || $('.tippedOff_msg_fixed').is(':visible'))
+	{	// already visible; don't show again
+		if( wait == false )
+			 $('.tippedOff_msg, .tippedOff_msg_fixed').hide();
+		return;
+	}
+
+	if(wait !== false )
 	{
 		tippedOff_timer = setTimeout(function()
 		{
 			if($('.tippedOff_msg').is(':visible')){$('.tippedOff_msg').hide();}
-			$msg_elem.css({"top":elPos.setting_top, "left":elPos.setting_left}).show();
-			tippedOffMsgElemPos($msg_elem, elPos)
+			if($('.tippedOff_msg_fixed').is(':visible')){$('.tippedOff_msg_fixed').hide().empty();}
+
+			if( $elem.data('showFixed') == false )
+			{
+				$msg_elem.css({"top":elPos.setting_top, "left":elPos.setting_left, "position":position}).show();
+				tippedOffMsgElemPos($msg_elem, elPos)
+			}
+			else
+			{
+				tippedOffShowFixedEl(wait, elPos)
+			}
 		}, wait);
 	}
 	else
 	{
 		if($('.tippedOff_msg').is(':visible')){$('.tippedOff_msg').hide();}
-		$msg_elem.css({"top":elPos.setting_top, "left":elPos.setting_left}).show();
-		tippedOffMsgElemPos($msg_elem, elPos)
+		if($('.tippedOff_msg_fixed').is(':visible')){$('.tippedOff_msg_fixed').hide().empty();}
+		if( $elem.data('showFixed') == false )
+		{
+			$msg_elem.css({"top":elPos.setting_top, "left":elPos.setting_left, "position":position}).show();
+			tippedOffMsgElemPos($msg_elem, elPos)
+		}
+		else
+		{
+			tippedOffShowFixedEl(wait, elPos)
+		}
 	}
 
 }
-
+function tippedOffShowFixedEl(wait, elPos)
+{
+	$msg_fixed_elem=$('.tippedOff_msg_fixed');
+	if(wait !== false)
+	{
+		tippedOff_timer = setTimeout(function()
+		{
+			if($('.tippedOff_msg').is(':visible')){$('.tippedOff_msg').hide();}
+			if($('.tippedOff_msg_fixed').is(':visible')){$('.tippedOff_msg_fixed').hide().empty();}
+			$msg_fixed_elem.css({"top":elPos.setting_top, "left":elPos.setting_left}).html($msg_elem.html()).show();
+			tippedOffMsgElemPos($msg_fixed_elem, elPos)
+		}, wait);
+	}
+	else
+	{
+		if($('.tippedOff_msg').is(':visible')){$('.tippedOff_msg').hide();}
+		if($('.tippedOff_msg_fixed').is(':visible')){$('.tippedOff_msg_fixed').hide().empty();}
+		$msg_fixed_elem.css({"top":elPos.setting_top, "left":elPos.setting_left}).html($msg_elem.html()).show();
+		tippedOffMsgElemPos($msg_fixed_elem, elPos)
+	}
+}
 function clearTippedOffTimer()
 {
 	clearTimeout(tippedOff_timer);
@@ -159,14 +217,14 @@ function tippedOffHide(wait)
 	{
 		tippedOff_timer = setTimeout(function()
 		{
-			$('.tippedOff_msg').hide();
+			$('.tippedOff_msg, .tippedOff_msg_fixed').hide();
 			clearTippedOffTimer();
 		}
 		, wait);
 	}
 	else
 	{
-		$('.tippedOff_msg').hide();
+		$('.tippedOff_msg, .tippedOff_msg_fixed').hide();
 	}
 }
 
@@ -175,7 +233,7 @@ function tippedOffElemPosition(elem)
 	var $elem = elem;
 	var $msg_elem = $elem.next('.tippedOff_msg');
 	var position = $elem.position();
-	var offset = $elem.offset();
+	var offset = $elem.offset()
 
 	var output = {};
 	var win_height = $(window).height();
@@ -192,28 +250,19 @@ function tippedOffElemPosition(elem)
 	output.elem_right_bottom = position.top + output.elem_height;//right bottom
 	output.elem_offset_left = offset.left;
 	output.elem_offset_top = offset.top;
+	output.elem_offset_right = offset.left + output.elem_width;
+	output.elem_offset_bottom = offset.top + output.elem_height;
 	//final position (default)
-	output.setting_top = output.elem_left_bottom;
-	output.setting_left = output.elem_left_top;
-
-/*
-	debugStr = '';
-	debugStr += 'ref = '+$elem.data('ref')+'<br>';
-	debugStr += 'win_height = '+win_height+'<br>';
-	debugStr += 'win_width = '+win_width+'<br>';
-	debugStr += 'output.elem_width = '+output.elem_width+'<br>';
-	debugStr += 'output.elem_height = '+output.elem_height+'<br>';
-	debugStr += 'output.elem_left_top = '+output.elem_left_top+'<br>';
-	debugStr += 'output.elem_left_bottom = '+output.elem_left_bottom+'<br>';
-	debugStr += 'output.elem_right_top = '+output.elem_right_top+'<br>';
-	debugStr += 'output.elem_right_bottom = '+output.elem_right_bottom+'<br>';
-	debugStr += 'output.elem_offset_left = '+output.elem_offset_left+'<br>';
-	debugStr += 'output.elem_offset_top = '+output.elem_offset_top+'<br>';
-	debugStr += 'output.setting_top = '+output.setting_top+'<br>';
-	debugStr += 'output.setting_left = '+output.setting_left+'<br><br>';
-	$('#debug').html(debugStr);
-*/
-
+	if($elem.data('showFixed') == false)
+	{
+		output.setting_top = output.elem_left_bottom;
+		output.setting_left = output.elem_left_top;
+	}
+	else
+	{
+		output.setting_top = output.elem_offset_bottom;
+		output.setting_left = output.elem_offset_top;
+	}
 	return output;
 }
 function tippedOffMsgElemPos(msg_elem, elPos)
@@ -225,11 +274,11 @@ function tippedOffMsgElemPos(msg_elem, elPos)
 	var msg_height = $msg_elem.height();
 	var msg_right_side = msg_offset.left + msg_width;
 	var msg_bottom = msg_offset.top + msg_height;
-	//$('#debug').append('<br><br>msg_right_side = '+msg_right_side+'<br>msg_bottom = '+msg_bottom+'<br>elPos.elem_right_top ='+elPos.elem_right_top);
-
 	var final_top = elPos.setting_top;
 	var final_left = elPos.setting_left;
 
+
+/*
 	if(msg_right_side > elPos.win_width)
 	{
 		final_left = elPos.setting_left-(msg_width-elPos.elem_width);
@@ -238,12 +287,28 @@ function tippedOffMsgElemPos(msg_elem, elPos)
 	{
 		final_top = elPos.elem_offset_top-(msg_bottom+elPos.elem_height);
 	}
-	//$('#debug').append('<br>final_left = '+final_left);
+*/
 	$msg_elem.css({"top":final_top, "left":final_left}).show();
 }
+
+function tippedOffFindRefs(selector)
+{
+//tippedOffFindRefs('#selector') or tippedOffFindRefs('.selector')
+//this will scan the UI for the selectors then store them in a temp Array for reuse
+	tippedOff_refArr = new Array();
+	$(selector).each(function()
+	{
+		tippedOff_refArr.push($(this).data('ref'));
+	});
+}
+(function($)
+{
+    $(document).mousemove(function(event) {
+        currentMousePos.x = event.clientX//event.pageX;
+        currentMousePos.y = event.clientY//event.pageY;
+    });
+})(jQuery);
 $(document).ready(function()
 {
-
 	$('.tipped_off').tippedOff();
-
 });
